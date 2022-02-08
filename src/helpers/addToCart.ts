2f2d234543660836608f4d782401f98af66c5e95
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-08 18:50:34 -0300

    :sparkles: introduce checkout
    introduce checkout with bakcend communication

--- src/helpers/addToCart.ts
+++ src/helpers/addToCart.ts
@@ -2,9 +2,11 @@ import { ProductType } from '../pages/Products/ProductCard';
 
 export default (product: ProductType) => {
   const productToAdd = { cont: 1, ...product };
+  const { email } = JSON.parse(localStorage.user);
+
   try {
-    if (localStorage && localStorage.checkout) {
-      const storage = JSON.parse(localStorage.checkout);
+    if (localStorage && localStorage[`checkout_${email}`]) {
+      const storage = JSON.parse(localStorage[`checkout_${email}`]);
       const { products } = storage;
       const exists = products.find(
         (item: ProductType) => item._id === product._id
@@ -13,8 +15,9 @@ export default (product: ProductType) => {
       if (exists) {
         exists.cont += 1;
         localStorage.setItem(
-          'checkout',
+          `checkout_${email}`,
           JSON.stringify({
+            ...storage,
             total: storage.total + productToAdd.price,
             products: [
               ...products.slice(0, products.indexOf(exists)),
@@ -25,8 +28,9 @@ export default (product: ProductType) => {
         );
       } else {
         localStorage.setItem(
-          'checkout',
+          `checkout_${email}`,
           JSON.stringify({
+            ...storage,
             total: storage.total + productToAdd.price,
             products: [...products, productToAdd]
           })
@@ -34,8 +38,12 @@ export default (product: ProductType) => {
       }
     } else {
       localStorage.setItem(
-        'checkout',
-        JSON.stringify({ total: productToAdd.price, products: [productToAdd] })
+        `checkout_${email}`,
+        JSON.stringify({
+          total: productToAdd.price,
+          products: [productToAdd],
+          user: email
+        })
       );
     }
   } catch (err) {
--- src/pages/User/Checkout.tsx
+++ src/pages/User/Checkout.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Button, Form, Offcanvas, Row } from 'react-bootstrap';
+import { Button, Form, Modal, Offcanvas, Row, Spinner } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 import { ProductType } from '../Products/ProductCard';
 import CheckoutCard from './CheckoutCard';
 
@@ -23,61 +25,118 @@ function Checkout({ show, onHide }: CheckoutProps) {
   const [address, setAddress] = useState('');
   const [products, setProducts] = useState([]);
   const [total, setTotal] = useState(0);
+  const [checkoutError, setCheckoutError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
+    const { email } = JSON.parse(localStorage.user);
     try {
-      if (localStorage && localStorage.checkout) {
-        const { products: productsList, total: totalPrice } = JSON.parse(
-          localStorage.checkout
-        );
-        setProducts(productsList);
-        setTotal(totalPrice);
+      if (localStorage && localStorage[`checkout_${email}`]) {
+        const {
+          products: productsList,
+          total: totalPrice,
+          user
+        } = JSON.parse(localStorage[`checkout_${email}`]);
+        if (user === email) {
+          setProducts(productsList);
+          setTotal(totalPrice);
+        }
       }
     } catch (err) {
       console.error(err);
     }
   }, [show]);
 
+  const checkOut = () => {
+    const { _id: id, token, email } = JSON.parse(localStorage.user);
+    if (address && products) {
+      setLoading(true);
+      axios
+        .post(
+          'https://eatflavor-bd.herokuapp.com/sales',
+          {
+            address,
+            total_price: total,
+            sale_date: new Date(),
+            status: 'pending',
+            products,
+            user_id: id
+          },
+          { headers: { authorization: token } }
+        )
+        .then(r => {
+          navigate(`/user/${r.data._id}/track`);
+          onHide(false);
+          setLoading(false);
+          localStorage.removeItem(`checkout_${email}`);
+          setAddress('');
+        })
+        .catch(() => console.log('erro'));
+    } else {
+      setCheckoutError(true);
+      setTimeout(() => setCheckoutError(false), 3000);
+    }
+  };
+
   return (
-    <Offcanvas
-      show={show}
-      onHide={onHide}
-      placement="end"
-      style={{ width: '32rem' }}
-    >
-      <Offcanvas.Header>
-        <Offcanvas.Title>
-          <h1 className="fw-bold">Finalizar pedido</h1>
-        </Offcanvas.Title>
-      </Offcanvas.Header>
-      <Offcanvas.Body>
-        <Row className="h-50 m-auto">
-          {products.length !== 0 &&
-            products.map((product: ProductType) => (
-              <CheckoutCard product={product} key={product.name} />
-            ))}
-        </Row>
-        <Row className="m-auto mt-5" style={{ height: '200px' }}>
-          <Row className="h-50 p-0 m-auto">
-            <Form>
-              <Form.Group>
-                <Form.Label>Endereço</Form.Label>
-                <Form.Control
-                  value={address}
-                  onChange={({ target }) => setAddress(target.value)}
-                  type="text"
-                  placeholder="Bairro, Rua e Número"
-                />
-              </Form.Group>
-            </Form>
-          </Row>
+    <>
+      <Modal show={checkoutError}>
+        <Modal.Body>
+          Não foi possível concluir a compra, confira seu endereço e produtos
+        </Modal.Body>
+      </Modal>
+      <Offcanvas
+        show={show}
+        onHide={onHide}
+        placement="end"
+        style={{ width: '32rem' }}
+      >
+        <Offcanvas.Header>
+          <Offcanvas.Title>
+            <h1 className="fw-bold">Finalizar pedido</h1>
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
           <Row className="h-50 m-auto">
-            <h2 className="fw-bold">Total: € {total.toFixed(2)}</h2>
-            <Button variant="success">Finalizar compra</Button>
+            {products.length !== 0 &&
+              products.map((product: ProductType) => (
+                <CheckoutCard product={product} key={product.name} />
+              ))}
           </Row>
-        </Row>
-      </Offcanvas.Body>
-    </Offcanvas>
+          <Row className="m-auto mt-5" style={{ height: '200px' }}>
+            <Row className="h-50 p-0 m-auto">
+              <Form>
+                <Form.Group>
+                  <Form.Label>Endereço</Form.Label>
+                  <Form.Control
+                    value={address}
+                    onChange={({ target }) => setAddress(target.value)}
+                    type="text"
+                    placeholder="Bairro, Rua e Número"
+                  />
+                </Form.Group>
+              </Form>
+            </Row>
+            <Row className="h-50 m-auto">
+              <h2 className="fw-bold">Total: € {total.toFixed(2)}</h2>
+              <Button variant="success" onClick={() => checkOut()}>
+                Finalizar compra
+              </Button>
+              {loading && (
+                <Spinner
+                  animation="border"
+                  role="status"
+                  className="m-auto mt-5"
+                >
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              )}
+            </Row>
+          </Row>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 }
 
