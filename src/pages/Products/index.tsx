ddcf5c79188d72b6503b02b4a8eeb96e338996a9
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-08 12:52:37 -0300

    :lipstick: style header
    all styles for user header

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,16 +1,40 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Outlet } from 'react-router';
+import {
+  BsFillHouseFill,
+  BsReceipt,
+  BsBagFill,
+  BsBoxArrowRight
+} from 'react-icons/bs';
+import logo from '../../images/eat_Flavor-black.png';
 
 function Products() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="white" expand="lg" className="shadow-sm">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>dd</Nav.Link>
-            <Nav.Link>d</Nav.Link>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="80"
+              height="80"
+              className="d-inline-block align-top"
+              alt="Eat Flavor logo"
+            />
+          </Navbar.Brand>
+          <Nav className="ml-auto d-flex w-25 justify-content-between">
+            <Nav.Link>
+              <BsFillHouseFill size={40} className="text-dark" />
+            </Nav.Link>
+            <Nav.Link>
+              <BsReceipt size={40} className="text-dark" />
+            </Nav.Link>
+            <Nav.Link>
+              <BsBagFill size={40} className="text-dark" />
+            </Nav.Link>
+            <Nav.Link>
+              <BsBoxArrowRight size={40} className="text-dark" />
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
