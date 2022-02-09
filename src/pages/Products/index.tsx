dbb8af2c8e0fbbed112e232e409accf476455e3b
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-09 01:58:03 -0300

    :adhesive_bandage: changing word
    changin dia for semana

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -33,7 +33,7 @@ function Products() {
     <Container fluid="lg" className="d-grid" style={{ height: '866px' }}>
       <Row className="w-100 pt-5 mt-5" style={{ height: '50vh' }}>
         <Row className="m-auto">
-          <h1 className="fw-bold mb-4">Menu do dia</h1>
+          <h1 className="fw-bold mb-4">Menu da semana</h1>
         </Row>
         <Row
           className="m-auto d-flex justify-content-between"
