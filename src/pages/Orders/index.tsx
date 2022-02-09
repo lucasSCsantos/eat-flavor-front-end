3ac7550b060a3c44686583adb4365297219a2fdc
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-08 21:27:05 -0300

    :bug: fix orders list bug
    fix orders list bug that was rendering all orders

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -8,17 +8,23 @@ function Orders() {
   const [sales, setSales] = useState([]);
 
   useEffect(() => {
-    const { token } = JSON.parse(localStorage.user);
+    const { token, _id: id } = JSON.parse(localStorage.user);
+    const getUserSales = (array: Array<OrderType>) =>
+      array.filter(arr => arr.user_id === id);
 
     axios
       .get('https://eatflavor-bd.herokuapp.com/sales', {
         headers: { authorization: token }
       })
       .then(r => {
-        setSales(r.data.sales);
+        const salesList = getUserSales(r.data.sales);
+        console.log(salesList);
+        setSales(salesList as never[]);
       })
       .catch(() => console.log('erro'));
   }, []);
+
+  useEffect(() => {}, []);
 
   return (
     <Container fluid="lg" className="d-grid" style={{ height: '866px' }}>
